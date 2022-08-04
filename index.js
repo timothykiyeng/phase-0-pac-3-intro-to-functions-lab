94808e23abd1f48280183eab067ae7902c4993db
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-04 15:23:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ console.log(string.toLowerCase());
         if(string === string.toLowerCase()){
             return "I can't hear you!"
          }
-        else if (string === string.toUpperCase){
+        else if (string === string.toUpperCase()){
             return "YES INDEED!"
          }
          else {
